869c9fb427c56211b70ef5a2af98d800894f7022
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2023-12-31 22:25:57 +0000

    Add loading spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,31 +1,33 @@
 import { useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import Spinner from "./shared/Spinner";
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
 
 const FeedbackList = () => {
-  const { feedback } = useContext(FeedbackContext);
-  return (
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No feedback Yet</p>;
+  }
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <>
-      {!feedback || feedback.length === 0 ? (
-        <p>No feedback Yet</p>
-      ) : (
-        <div className="feedback-list">
-          <AnimatePresence>
-            {feedback.map((item) => (
-              <motion.div
-                key={item.id}
-                // whileHover={{ scale: 1.1 }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <FeedbackItem key={item.id} item={item} />
-              </motion.div>
-            ))}
-          </AnimatePresence>
-        </div>
-      )}
+      <div className="feedback-list">
+        <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div
+              key={item.id}
+              // whileHover={{ scale: 1.1 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem key={item.id} item={item} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </div>
     </>
   );
 };

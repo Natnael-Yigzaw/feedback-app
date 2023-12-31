3b097b489c48128289359b76e5106e9cdd49226f
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2023-12-31 11:18:12 +0000

    Add Framer Motion animations to FeedbackItem component.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 
@@ -8,13 +9,23 @@ const FeedbackList = ({ feedback, handledelete }) => {
         <p>No feedback Yet</p>
       ) : (
         <div className="feedback-list">
-          {feedback.map((item) => (
-            <FeedbackItem
-              key={item.id}
-              item={item}
-              handleDelete={handledelete}
-            />
-          ))}
+          <AnimatePresence>
+            {feedback.map((item) => (
+              <motion.div
+                key={item.id}
+                whileHover={{ scale: 1.1 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <FeedbackItem
+                  key={item.id}
+                  item={item}
+                  handleDelete={handledelete}
+                />
+              </motion.div>
+            ))}
+          </AnimatePresence>
         </div>
       )}
     </>
